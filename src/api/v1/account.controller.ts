8681821c5a0c189e3adf763fb9bed7d9f1a552d8
Fenish <sohretalhadev@outlook.com>
2023-09-19 01:23:10 +0300

    Rename controller

--- src/api/v1/account.controller.ts
+++ src/api/v1/account.controller.ts
@@ -1,12 +1,12 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientProxy, MessagePattern } from '@nestjs/microservices';
-import { USER_SERVICE } from '../../utils/constants';
-import { UserService } from './user.service';
+import { ACCOUNT_SERVICE } from '../../utils/constants';
+import { UserService } from './account.service';
 
 @Controller('v1/user')
 export class UserController {
   constructor(
-    @Inject(USER_SERVICE) private readonly user_service: ClientProxy,
+    @Inject(ACCOUNT_SERVICE) private readonly user_service: ClientProxy,
     private readonly userService: UserService,
   ) {}
 
