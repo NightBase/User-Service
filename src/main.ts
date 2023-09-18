d33674e2ef2867fa6ba2c30dc7562640e6f35bbb
Fenish <sohretalhadev@outlook.com>
2023-09-18 20:59:30 +0300

    Add microservice connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import dotenv from 'dotenv';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      retryAttempts: 5,
+      retryDelay: 3000,
+      port: 3000,
+    },
+  });
+  app.enableCors();
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT || 5000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
