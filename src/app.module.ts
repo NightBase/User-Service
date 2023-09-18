24a4a2973148d434d242ac1577661cb6a3d1104e
Fenish <sohretalhadev@outlook.com>
2023-09-18 20:59:21 +0300

    Add user controller to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+// V1
+import { UserController } from './api/v1/user.controller';
+import { UserService } from './api/v1/user.service';
+// V1 Middleware
+import { CheckCredentials } from './api/v1/middleware/account';
+
+// Services
+import { USER_SERVICE } from './utils/constants';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ClientsModule.register([{ name: USER_SERVICE, transport: Transport.TCP }]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CheckCredentials).forRoutes({
+      path: 'v1/user',
+      method: RequestMethod.POST,
+    });
+  }
+}
