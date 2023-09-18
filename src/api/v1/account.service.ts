9ab619b549dedfcabe7a24a9966db93b5bf5d006
Fenish <sohretalhadev@outlook.com>
2023-09-19 01:23:03 +0300

    Rename User.service

--- src/api/v1/account.service.ts
+++ src/api/v1/account.service.ts
@@ -1,11 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { USER_SERVICE } from '../../utils/constants';
+import { ACCOUNT_SERVICE } from '../../utils/constants';
 
 @Injectable()
 export class UserService {
   constructor(
-    @Inject(USER_SERVICE) private readonly user_service: ClientProxy,
+    @Inject(ACCOUNT_SERVICE) private readonly user_service: ClientProxy,
   ) {}
 
   createUser(data: any) {
