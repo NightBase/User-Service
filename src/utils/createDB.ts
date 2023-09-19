0f60021fd7004c425762fce0143d9d433584ce63
Fenish <sohretalhadev@outlook.com>
2023-09-19 03:04:14 +0300

    Create database creator

---
+++ src/utils/createDB.ts
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+
+dotenv.config();
+
+const dbName = 'NightBase-User';
+
+const sequelize = new Sequelize({
+  database: 'postgres', // Connect to the 'postgres' database first
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  dialect: 'postgres',
+});
+
+// Create the database if it doesn't exist
+sequelize
+  .query(`SELECT 1 FROM pg_database WHERE datname = '${dbName}'`)
+  .then(([results]) => {
+    if (!results.length) {
+      return sequelize.query(`CREATE DATABASE "${dbName}"`);
+    }
+  })
+  .then(() => {
+    console.log(`Database '${dbName}' created or already exists.`);
+  })
+  .catch((err) => {
+    console.error('Error creating database:', err);
+  })
+  .finally(() => {
+    sequelize.close();
+  });
