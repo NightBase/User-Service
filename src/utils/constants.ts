84adfc01e56bc1efdf38f8656fa67b0a372a0a33
Fenish <sohretalhadev@outlook.com>
2023-09-19 17:14:29 +0300

    Remove HTTP_STATUS from constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,16 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const ACCOUNT_SERVICE = 'ACCOUNT_SERVICE';
-
-export enum HTTP_STATUS {
-  OK = 200,
-  CREATED = 201,
-  BAD_REQUEST = 400,
-  UNAUTHORIZED = 401,
-  NOT_FOUND = 404,
-  INTERNAL_SERVER_ERROR = 500,
-}
+export const ACCOUNT_SERVICE_NAME = 'ACCOUNT_SERVICE_NAME';
+export const ACCOUNT_QUEUE_NAME = 'NightBase:Account';
 
 export const BROKERS = [
   `amqp://${process.env.BROKER_HOST}:${process.env.BROKER_PORT}`,
