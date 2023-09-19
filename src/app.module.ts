283ad534c0f652cab5e10168730bfc226588000a
Fenish <sohretalhadev@outlook.com>
2023-09-20 00:13:02 +0300

    Remove token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AccountModule } from './api/v1/account/account.module';
-import { TokenModule } from './api/v1/token/token.module';
 import { JwtModule } from '@nestjs/jwt';
 import { DATABASE_NAME } from './utils/constants';
 
 @Module({
   imports: [
     AccountModule,
-    TokenModule,
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.DB_HOST,
