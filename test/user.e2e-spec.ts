719211280943f3fad33eeb58988689349eeffe6d
Fenish <sohretalhadev@outlook.com>
2023-09-19 19:48:07 +0300

    Create tests

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from '../src/app.module';
 describe('User Service V1', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,8 +15,49 @@ describe('User Service V1', () => {
     await app.init();
   });
 
-  it('[POST] Create User Without Credentials', () => {
-    return request(app.getHttpServer()).post('/v1/user').expect(400);
+  it('[POST] Should return 400 when creating a user without credentials', async () => {
+    await request(app.getHttpServer()).post('/v1/user').expect(400);
+  });
+
+  it('[POST] Should successfully create a root user', async () => {
+    const credentials = {
+      username: 'JohnDoe',
+      email: 'JohnDoe@example.com',
+      password: '12345',
+      isRoot: 'true',
+    };
+
+    await request(app.getHttpServer())
+      .post('/v1/user')
+      .send(credentials)
+      .expect(201);
+  });
+
+  it('[POST] Should return 400 when creating a 2nd root user', async () => {
+    const credentials = {
+      username: 'JohnDoe2',
+      email: 'JohnDoe2@example.com',
+      password: '12345',
+      isRoot: 'true',
+    };
+
+    await request(app.getHttpServer())
+      .post('/v1/user')
+      .send(credentials)
+      .expect(400);
+  });
+
+  it('[POST] Should return 401 when creating a user without authorization', async () => {
+    const credentials = {
+      username: 'JohnDoe2',
+      email: 'JohnDoe2@example.com',
+      password: '12345',
+    };
+
+    await request(app.getHttpServer())
+      .post('/v1/user')
+      .send(credentials)
+      .expect(401);
   });
 
   afterAll(async () => {
