83353de648f75dfaff3b4fc575755cb189e260be
Fenish <sohretalhadev@outlook.com>
2023-09-21 19:42:05 +0300

    Change name of event

--- src/api/v1/account/account.controller.ts
+++ src/api/v1/account/account.controller.ts
@@ -1,14 +1,11 @@
-import { Body, Controller, HttpCode, Inject, Post } from '@nestjs/common';
-import { ClientProxy, MessagePattern } from '@nestjs/microservices';
-import { ACCOUNT_SERVICE_NAME } from '@/utils/constants';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { AccountService } from './account.service';
+import { Credentials } from '../Database/Dto/create-account';
 
 @Controller('v1/user')
 export class AccountController {
-  constructor(
-    @Inject(ACCOUNT_SERVICE_NAME) private readonly accountQueue: ClientProxy,
-    private readonly accountService: AccountService,
-  ) {}
+  constructor(private readonly accountService: AccountService) {}
 
   @Post()
   @HttpCode(201)
@@ -16,8 +13,8 @@ export class AccountController {
     return this.accountService.createUser(body);
   }
 
-  @MessagePattern({ cmd: 'createUser' })
-  async createUserHandler(@Body() body) {
-    return this.accountService.createUser(body);
+  @MessagePattern('Queen:CreateUser')
+  async createUserHandler(data: Credentials) {
+    return this.accountService.createUser(data);
   }
 }
