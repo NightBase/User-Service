5141be5049e4f3b3fd11eb94dfa1377d6559251d
Fenish <sohretalhadev@outlook.com>
2023-09-22 03:04:04 +0300

    Change Database name

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const DATABASE_NAME = 'NightBase-Account';
+export const DATABASE_NAME = 'NB:User';
 
 export const ACCOUNT_SERVICE_NAME = 'ACCOUNT_SERVICE';
 export const ACCOUNT_QUEUE_NAME = 'NightBase:Account';
