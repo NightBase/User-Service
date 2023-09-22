be2b74f1016f7ab637315e8c7156cd2a481a3a00
Fenish <sohretalhadev@outlook.com>
2023-09-22 03:04:13 +0300

    Remove pre data

--- src/utils/setupDB.ts
+++ src/utils/setupDB.ts
@@ -2,8 +2,6 @@ import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize-typescript';
 import { DATABASE_NAME } from './constants';
 
-import { Permission } from '../api/v1/Database/Models/role.model';
-
 dotenv.config();
 
 const dbName = DATABASE_NAME;
@@ -30,58 +28,8 @@ async function createDatabase() {
   await sequelize.close();
 }
 
-async function insertPreData() {
-  const sequelize = new Sequelize({
-    database: dbName,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    dialect: 'postgres',
-    logging: false,
-  });
-  sequelize.addModels([Permission]);
-  await sequelize.sync({ alter: true });
-  await Permission.destroy({ truncate: true });
-  await Permission.bulkCreate([
-    {
-      name: 'Read',
-      description: 'To read data from the database',
-      alias: 'READ',
-    },
-    {
-      name: 'Write',
-      description: 'To write data to the database',
-      alias: 'WRITE',
-    },
-    {
-      name: 'Update',
-      description: 'To update data in the database',
-      alias: 'UPDATE',
-    },
-    {
-      name: 'Delete Data',
-      description: 'To delete data from the database',
-      alias: 'DELETE',
-    },
-    {
-      name: 'Create Table',
-      description: 'To create a new table in the database',
-      alias: 'CREATE_TABLE',
-    },
-    {
-      name: 'Drop Table',
-      description: 'To drop a table from the database',
-      alias: 'DROP_TABLE',
-    },
-  ]);
-  console.log('Predefined data inserted.');
-  await sequelize.close();
-}
-
 async function setupDB() {
   await createDatabase();
-  await insertPreData();
 }
 
 setupDB();
