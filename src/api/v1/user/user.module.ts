f30b523263ab2d03451b1ac548965ce73f62949b
Fenish <sohretalhadev@outlook.com>
2023-09-27 19:54:38 +0300

    Remove middlewares

--- src/api/v1/user/user.module.ts
+++ src/api/v1/user/user.module.ts
@@ -1,28 +1,16 @@
 import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import {
-  BROKERS,
-  AUTHENTICATION_SERVICE_NAME,
   AUTHENTICATION_QUEUE_NAME,
+  AUTHENTICATION_SERVICE_NAME,
+  BROKERS,
 } from '@/utils/constants';
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { SequelizeModule } from '@nestjs/sequelize';
 
-// V1
-import { UserController } from './user.controller';
-import { UserCreateService } from './create/create.service';
-
-// V1 Middleware
-import { CheckCredentials } from '../middleware/create.middleware';
-
-// Models
 import { User } from '../../common/Database/Models/user.model';
-import { AuthRequired } from '../middleware/auth.middleware';
+import { UserCreateService } from './create/create.service';
 import { UserGetService } from './get/get.service';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [
@@ -44,15 +32,4 @@ import { UserGetService } from './get/get.service';
   controllers: [UserController],
   providers: [UserCreateService, UserGetService],
 })
-export class UserModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(CheckCredentials).forRoutes({
-      path: 'v1/user',
-      method: RequestMethod.POST,
-    });
-    consumer.apply(AuthRequired).forRoutes({
-      path: 'v1/user/*',
-      method: RequestMethod.GET,
-    });
-  }
-}
+export class UserModule {}
