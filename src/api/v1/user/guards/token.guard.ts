3dde62f4cef3e2a28ed6c7d2fd6adff906aceac1
Fenish <sohretalhadev@outlook.com>
2023-09-27 19:56:15 +0300

    Add token refresher

--- src/api/v1/user/guards/token.guard.ts
+++ src/api/v1/user/guards/token.guard.ts
@@ -30,3 +30,71 @@ export class CookieGuard implements CanActivate {
     return true;
   }
 }
+
+export class TokenRefreshGuard implements CanActivate {
+  constructor(
+    @Inject(AUTHENTICATION_SERVICE_NAME)
+    private readonly authQueue: ClientProxy,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
+
+    const accessToken = request.cookies['accessToken'];
+
+    await this.isTokenValid(accessToken);
+    const isExpired = await this.isTokenExpired(accessToken);
+    if (!isExpired) return true;
+
+    const refreshToken = request.cookies['refreshToken'];
+    const newToken = await this.refreshToken(refreshToken);
+
+    request.cookies['accessToken'] = newToken;
+    response.cookie('accessToken', newToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      secure: true,
+    });
+
+    return true;
+  }
+
+  async isTokenValid(token: string) {
+    const isValid = await lastValueFrom(
+      this.authQueue.send('NB-Auth:IsTokenValid', token),
+    );
+    if (!isValid) {
+      throw new HttpException(
+        {
+          message: 'Token is invalid',
+          logout: true,
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    return isValid;
+  }
+
+  async isTokenExpired(token: string) {
+    const isExpired = await lastValueFrom(
+      this.authQueue.send('NB-Auth:IsTokenExpired', token),
+    );
+    return isExpired;
+  }
+
+  async refreshToken(refreshToken: string) {
+    const token = await lastValueFrom(
+      this.authQueue.send('NB-Auth:RefreshToken', refreshToken),
+    );
+    if (!token) {
+      throw new HttpException(
+        {
+          message: 'Token is invalid',
+          logout: true,
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    return token;
+  }
+}
