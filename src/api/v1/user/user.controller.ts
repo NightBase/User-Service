73689dc3a8c816172fa34530ad3afa3f480e1d85
Fenish <sohretalhadev@outlook.com>
2023-09-28 03:16:19 +0300

    Add delete user endpoint

--- src/api/v1/user/user.controller.ts
+++ src/api/v1/user/user.controller.ts
@@ -3,6 +3,7 @@ import { Request } from 'express';
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   Param,
@@ -13,16 +14,18 @@ import {
 import { MessagePattern } from '@nestjs/microservices';
 
 import { UserCreateService } from './create/create.service';
+import { UserDeleteService } from './delete/delete.service';
 import { UserGetService } from './get/get.service';
 import { CheckCredentials } from './guards/create.guard';
 import { CookieGuard, TokenRefreshGuard } from './guards/token.guard';
 
 @Controller('v1/user')
-@UseGuards(CookieGuard, TokenRefreshGuard)
+@UseGuards(TokenRefreshGuard)
 export class UserController {
   constructor(
     private readonly userCreateService: UserCreateService,
     private readonly userGetService: UserGetService,
+    private readonly userDeletService: UserDeleteService,
   ) {}
 
   @Post()
@@ -36,6 +39,7 @@ export class UserController {
 
   @Get('@me')
   @HttpCode(200)
+  @UseGuards(CookieGuard)
   getMe(@Req() req: Request) {
     const cookies = req.cookies;
     return this.userGetService.getMe(cookies['accessToken']);
@@ -43,8 +47,18 @@ export class UserController {
 
   @HttpCode(200)
   @Get(':username')
+  @UseGuards(CookieGuard)
   getUser(@Param() params: any) {
     const username = params.username;
     return this.userGetService.getUser(username);
+  }
+
+  @HttpCode(200)
+  @Delete(':username')
+  @UseGuards(CookieGuard)
+  deleteUser(@Param() params, @Req() req) {
+    const username = params.username;
+    const accessToken = req.cookies['accessToken'];
+    return this.userDeletService.deleteUser(username, accessToken);
   }
 }
